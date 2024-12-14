dd24fd4d8bdfe2cba6da16932c19fe2c5dffcc35
Fatimakhan26 <aashikhan654@gmail.com>
2024-12-14 15:17:57 +0500

    some changes

--- src/app/components/Frame.tsx
+++ src/app/components/Frame.tsx
@@ -1,25 +1,6 @@
 import React from 'react'
 import { FaLongArrowAltRight } from "react-icons/fa";
 
-// const Frame = () => {
-//   return (
-//     <div className='bg-[#F5F7FA]'>
-//       <h1 className='text-black font-bold text-5xl text-center p-10 pr-96 pl-96'>Pellentesque suscipit fringilla libero eu.</h1>
-//       <button className="bg-primary text-white p-2 pl-4 pr-4 ml-[600px]">
-//         <ul className='flex gap-2'>
-//             <li>Get a Demo</li>
-//             <li className='mt-1'><FaLongArrowAltRight/></li>
-//         </ul>
-//         </button>
-//     </div>
-//   )
-// }
-
-// export default Frame
-
-
-
-
 const Frame = () => {
   return (
     <div className="bg-[#F5F7FA] p-6">
